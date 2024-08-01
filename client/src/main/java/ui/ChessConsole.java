09bcfb2e28cf4d22ea996d24eadc5c7af504da8f
Alyssa Jones <alyssamj02@gmail.com>
2024-08-01 12:32:14 -0600

    Made progress on drawing the board

--- client/src/main/java/ui/ChessConsole.java
+++ client/src/main/java/ui/ChessConsole.java
@@ -6,16 +6,18 @@ import java.util.Random;
 
 import static ui.EscapeSequences.*;
 
+
 public class ChessConsole {
 
-    private static final int BOARD_SIZE_IN_SQUARES = 3;
+    private static final int BOARD_SIZE_IN_SQUARES = 8;
     private static final int SQUARE_SIZE_IN_PADDED_CHARS = 3;
     private static final int LINE_WIDTH_IN_PADDED_CHARS = 1;
 
     // Padded characters.
-    private static final String EMPTY = "   ";
-    private static final String X = " X ";
-    private static final String O = " O ";
+    private static final String EMPTY = " ";
+    private static final String X = "X";
+    private static final String O = "O";
+    private String[][] myBoard;
 
     private static Random rand = new Random();
 
@@ -38,21 +40,23 @@ public class ChessConsole {
         setBlack(out);
 
         String[] headers = { "a", "b", "c", "d", "e", "f", "g", "h"};
+        String[] leftSide = {"1", "2", "3", "4", "5", "6", "7", "8"};
+        out.print("    ");
         for (int boardCol = 0; boardCol < headers.length; ++boardCol) {
             drawHeader(out, headers[boardCol]);
-
         }
+        out.print(SET_BG_COLOR_BLACK);
 
         out.println();
     }
 
     private static void drawHeader(PrintStream out, String headerText) {
-        int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
-        int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
+        out.print(SET_BG_COLOR_RED);
+        out.print(SET_TEXT_COLOR_WHITE);
 
-        out.print(EMPTY.repeat(prefixLength));
-        printHeaderText(out, headerText);
-        out.print(EMPTY.repeat(suffixLength));
+        out.print(" ");
+        out.print(headerText);
+        out.print(" ");
     }
 
     private static void printHeaderText(PrintStream out, String player) {
@@ -72,7 +76,7 @@ public class ChessConsole {
 
             if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
                 // Draw horizontal row separator.
-                drawHorizontalLine(out);
+//                drawHorizontalLine(out);
                 setBlack(out);
             }
         }
@@ -80,48 +84,43 @@ public class ChessConsole {
 
     private static void drawRowOfSquares(PrintStream out) {
 
-        for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_PADDED_CHARS; ++squareRow) {
-            for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+        for (int squareRow = 0; squareRow < 8; squareRow++) {
+            out.print(" "+(squareRow + 1));
+            for (int boardCol = 0; boardCol < 8; boardCol++) {
                 setWhite(out);
-
                 if (squareRow == SQUARE_SIZE_IN_PADDED_CHARS / 2) {
-                    int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
-                    int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
-
-                    out.print(EMPTY.repeat(prefixLength));
-                    printPlayer(out, rand.nextBoolean() ? X : O);
-                    out.print(EMPTY.repeat(suffixLength));
+                    setColor(out, squareRow, boardCol);
+//                    if (boardCol % 2 == 0) {
+//                        setBlack(out);
+//                        out.print(EMPTY.repeat(prefixLength));
+//                        printDarkSquare(out, rand.nextBoolean() ? X : O);
+//                        out.print(EMPTY.repeat(suffixLength));
+//                        setBlack(out);
+//                    } else {
+//                        out.print(EMPTY.repeat(prefixLength));
+//                        printPlayer(out, rand.nextBoolean() ? X : O);
+//                        out.print(EMPTY.repeat(suffixLength));
+//                    }
                 }
-                else {
-                    out.print(EMPTY.repeat(SQUARE_SIZE_IN_PADDED_CHARS));
-                }
-
-                if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-                    // Draw vertical column separator.
-                    setBlue(out);
-                    out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
-                }
-
                 setBlack(out);
             }
-
-            out.println();
         }
+        out.println();
     }
 
-    private static void drawHorizontalLine(PrintStream out) {
+//    private static void drawHorizontalLine(PrintStream out) {
+//
+//        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_PADDED_CHARS +
+//                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_PADDED_CHARS;
+//
+//        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_PADDED_CHARS; ++lineRow) {
+//            setBlue(out);
+//            out.print(EMPTY.repeat(boardSizeInSpaces));
+//
+//            setBlack(out);
+//            out.println();
+//        }
 
-        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_PADDED_CHARS +
-                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_PADDED_CHARS;
-
-        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_PADDED_CHARS; ++lineRow) {
-            setBlue(out);
-            out.print(EMPTY.repeat(boardSizeInSpaces));
-
-            setBlack(out);
-            out.println();
-        }
-    }
 
     private static void setWhite(PrintStream out) {
         out.print(SET_BG_COLOR_WHITE);
@@ -138,13 +137,33 @@ public class ChessConsole {
         out.print(SET_TEXT_COLOR_BLACK);
     }
 
-    private static void printPlayer(PrintStream out, String player) {
+    private static void printLightSquare(PrintStream out, String player) {
         out.print(SET_BG_COLOR_WHITE);
         out.print(SET_TEXT_COLOR_BLACK);
 
         out.print(player);
 
         setWhite(out);
+    }
+
+    private static void setColor(PrintStream out, int row, int col) {
+        if ((row) %2 == 0) {
+            printDarkSquare(out, " O ");
+        } else {
+            printLightSquare(out, " X ");
+        }
+    }
+
+    private static void printDarkSquare(PrintStream out, String player) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_WHITE);
+
+        out.print(player);
+        setBlack(out);
+    }
+
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
 
